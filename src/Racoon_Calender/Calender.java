3fb3938e2b73bb8540e9b5df30496e019914e67a
potato_cat <potato_cat@218.38.137.27>
2022-02-22 16:15:41 +0900

    update maxdays

--- src/Racoon_Calender/Calender.java
+++ src/Racoon_Calender/Calender.java
@@ -3,25 +3,33 @@ package Racoon_Calender;
 import java.util.Scanner;
 
 public class Calender {
-
-	public static void main(String[] args) {
+	private static int[] Max_Days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30 ,31};
+	public int MaxdaysofMonth(int month) {
+		return Max_Days[month - 1];
+	}
+	
+	public void printSampleCalender() {
+		System.out.println("<Calender Sample>");
 		System.out.println("일  월  화  수 목 금  토");
 		System.out.println("--------------------");
 		System.out.println(" 1  2  3  4  5  6  7");
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
-		
+	}
+	
+	public static void main(String[] args) {
 		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
 		Scanner scanner = new Scanner(System.in);
+		Calender cal = new Calender();
 		System.out.println("달을 입력하세요");
 		int month = scanner.nextInt();
 		
-		int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30 ,31};
+		System.out.printf("%d월은 %d일까지 있습니다\n", month, cal.MaxdaysofMonth(month));
 		
-		System.out.printf("%d월은 %d일까지 있습니다\n", month, maxDays[month - 1]);
+		cal.printSampleCalender();
 		
-				
+		scanner.close();		
 	}
 
 }
