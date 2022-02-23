310e946faf5eacd424c35711f20e8d53d48cce55
potato_cat <potato_cat@218.38.137.27>
2022-02-23 19:38:29 +0900

    update Leap

--- src/Racoon_Calender/Calender.java
+++ src/Racoon_Calender/Calender.java
@@ -4,39 +4,51 @@ import java.util.Scanner;
 
 public class Calender {
 	private static int[] Max_Days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30 ,31};
-	public int MaxdaysofMonth(int month) {
-		
+	private static int[] Leap_Max_Days = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30 ,31};
+	
+	public boolean isLeapyear(int year) {
+		if(year%4 == 0 && (year%100 != 0 || year%400 == 0)) {
+			return true;
+		}else{
+		return false;
+	}
+	}
+	
+	public int MaxdaysofMonth(int year, int month) {
+		if(isLeapyear(year)) {
+			return Leap_Max_Days[month - 1];
+		}else {
 		return Max_Days[month - 1];
 	}
+	}
+	/**
+	 * - 월을 입력하면 해당 월의 달력을 출력한다
+	 * - 달력의 모양은 1단계에서 작성한 모양으로 만든다 
+	 * - 1일은 일요일도 정해도 무방하다
+	 * - -1을 입력받기 전까지 반복 입력받는다
+	 * - 프롬프트를 출력한다
+	 */
 	
-	public void printSampleCalender() {
-		System.out.println("<Calender Sample>");
-		System.out.println("일  월  화  수 목 금  토");
-		System.out.println("--------------------");
-		System.out.println(" 1  2  3  4  5  6  7");
-		System.out.println(" 8  9 10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
+	
+	public void printCalender(int year, int month) {
+		System.out.printf("   <<%4d년%3d월>>\n", year, month);
+		System.out.println(" SU MO TU WE TH FR SA");
+		System.out.println("---------------------");
+		
+		int maxDay = MaxdaysofMonth(year, month);
+		
+		for(int i=1; i<=maxDay; i++) {
+			System.out.printf("%3d", i);
+			if(i%7 == 0) {
+				System.out.println();
+			}
+		}
+		System.out.println();
+//		System.out.println(" 1  2  3  4  5  6  7");
+//		System.out.println(" 8  9 10 11 12 13 14");
+//		System.out.println("15 16 17 18 19 20 21");
+//		System.out.println("22 23 24 25 26 27 28");
 	}
 	
-	public static void main(String[] args) {
-		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
-		Scanner scanner = new Scanner(System.in);
-		Calender cal = new Calender();
-		
-		System.out.println("반복횟수를 입력하세요");
-		int repeat = scanner.nextInt();
-		
-		for(int i = 0; i<repeat; i++) {
-			System.out.println("달을 입력하세요");
-			int month = scanner.nextInt();
-			System.out.printf("%d월은 %d일까지 있습니다\n", month, cal.MaxdaysofMonth(month));
-					}
-		System.out.println("Bye!!");
 	
-//		cal.printSampleCalender();
-		
-		scanner.close();		
-	}
-
 }
---
+++ src/Racoon_Calender/Prompt.java
@@ -0,0 +1,47 @@
+package Racoon_Calender;
+
+import java.util.Scanner;
+
+public class Prompt {
+		private final static String PROMPT="cal> ";
+		
+		public void runPrompt() {
+			
+			Scanner scanner = new Scanner(System.in);
+			Calender cal = new Calender();
+			
+			int month = 1;
+			int year = 1;
+			
+			while(true) {
+				System.out.println("년도를 입력하세요");
+				System.out.print(PROMPT);
+				year = scanner.nextInt();
+				
+				System.out.println("달을 입력하세요");
+				System.out.print(PROMPT);
+				month = scanner.nextInt();
+				if(month == -1) {
+					break;
+				}
+				if(month > 12) {
+					continue;
+				}
+				cal.printCalender(year, month);
+						}
+			System.out.println("Bye!!");
+		
+			scanner.close();		
+		}
+	
+		public static void main(String[] args) {
+			//앱 실행
+			Prompt p = new Prompt();
+			p.runPrompt();
+						
+		}
+
+
+	}
+
+
